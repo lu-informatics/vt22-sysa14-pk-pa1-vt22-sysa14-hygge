b6aea623b993e8f6473db237540deee64804765b
karinengdal <karinlisa.engdal@gmail.com>
2022-02-16 12:02:59 +0000

    fixed my password

--- HyggeFinal/HyggeFinal/DataAccessLayer.cs
+++ HyggeFinal/HyggeFinal/DataAccessLayer.cs
@@ -11,7 +11,7 @@ namespace HyggeFinal
     {
         private static Dictionary<string, string> queries = new Dictionary<string,string>() {};
                                                                           /*SERVER NAME: VMKARIN / VMMAJA / SYST4DEV01*/
-        private static SqlConnection cnn = new SqlConnection("Data Source = SYST4DEV01; Initial Catalog = Hygge; User ID=hygge ; Password =hej123 "); // CHECK THIS IF CONNECT FAIL
+        private static SqlConnection cnn = new SqlConnection("Data Source = VMKARIN; Initial Catalog = Hygge; User ID=hygge ; Password =hej123 "); // CHECK THIS IF CONNECT FAIL
         
         /* update methods can open and close the method in one go, whilst find methods need to send the connection to be closed after saving the value-
         the reason that we don't handle the conversion inside the method is because it decreases flexibility and modularity; the data access layer should only return datareaders.
