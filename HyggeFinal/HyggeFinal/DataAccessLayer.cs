302d000817728d6009f3a9041096122d78b7630c
da1552hu-s <75209747+da1552hu-s@users.noreply.github.com>
2022-02-16 12:19:06 +0100

    comment

--- HyggeFinal/HyggeFinal/DataAccessLayer.cs
+++ HyggeFinal/HyggeFinal/DataAccessLayer.cs
@@ -10,6 +10,7 @@ namespace HyggeFinal
     public class DataAccessLayer
     {
         private static Dictionary<string, string> queries = new Dictionary<string,string>() {};
+                                                                          /*SERVER NAME*/
         private static SqlConnection cnn = new SqlConnection("Data Source = SYST4DEV01; Initial Catalog = Hygge; User ID=hygge ; Password =hej123 "); // CHECK THIS IF CONNECT FAIL
         
         /* update methods can open and close the method in one go, whilst find methods need to send the connection to be closed after saving the value-
