2a93b1d9925e7143b381f73cd2d42d90d575ceb1
da1552hu-s <75209747+da1552hu-s@users.noreply.github.com>
2022-02-18 18:12:52 +0100

    new method call system

--- HyggeFinal/HyggeFinal/DataAccessLayer.cs
+++ HyggeFinal/HyggeFinal/DataAccessLayer.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Data;
+using System.Collections.Generic;
 
 namespace HyggeFinal
 {
     public class DataAccessLayer {
+
+        public static string Test()
+        { //this method should only be used to test out new features of the db. it does not test the functionality of the whole class.
+            try
+            {
+                DataSet ds = Login.ReadLogin("anabanana@hotmail.com");
+                return ds.Tables[0].Rows[0][0].ToString(); // returns the value of the first column of the first row in string format
+            }
+            catch (SqlException) { return ("failed to connect."); } // returns an error message if the client couldn't connect to the db server (check the data source!)
+        }
 
         public enum Table
         {
@@ -16,26 +27,28 @@ namespace HyggeFinal
             Education
         }
 
+        private static class Utils { //These methods partially define the SQL query, leaving parameter fill for the SendToDatabes method.
+            // IMPORTANT: These methods requires ParamIDs to match parameter names in the database!
 
-        private static class Utils {
-            public static DataSet Read(Table table, ParamArgs primaryKey) //A table and ParamArgs object is passed to partially define search query, leaving parameter fill to SendToDatabase()
-                => SendToDatabase($"SELECT * FROM {table} WHERE {primaryKey.ParamID.Substring(1)} = {primaryKey.ParamID}",primaryKey);
-
-            public static void Delete(Table table, ParamArgs primaryKey)
-                => SendToDatabase($"DELETE {table} WHERE {primaryKey.ParamID.Substring(1)} = {primaryKey.ParamID}",primaryKey);
-
-            public static void Update(Table table, ParamArgs primaryKey, ParamArgs changedValue) // IMPORTANT: This method requires ParamID to match parameter name in the database!
+            //CREATE TEMPLATE
+            public static void Create(Table table, ParamArgs value1, ParamArgs value2) 
+                => SendToDatabase($"INSERT INTO {table}({value1.ParamID.Substring(1)},{value2.ParamID.Substring(1)}) VALUES ({value1.ParamID},{value2.ParamID})",value1,value2);
+            //UPDATE TEMPLATE
+            public static void Update(Table table, ParamArgs primaryKey, ParamArgs changedValue) 
                 => SendToDatabase($"UPDATE {table} SET {changedValue.ParamID.Substring(1)} = {changedValue.ParamID} WHERE {primaryKey.ParamID.Substring(1)} = {primaryKey.ParamID}",
                     changedValue,
                     primaryKey);
+            //READ TEMPLATE
+            public static DataSet Read(Table table, ParamArgs primaryKey)
+                => SendToDatabase($"SELECT * FROM {table} WHERE {primaryKey.ParamID.Substring(1)} = {primaryKey.ParamID}",primaryKey);
+            //DELETE TEMPLATE
+            public static void Delete(Table table, ParamArgs primaryKey)
+                => SendToDatabase($"DELETE {table} WHERE {primaryKey.ParamID.Substring(1)} = {primaryKey.ParamID}",primaryKey);
         }
-
 
         public static class Login {
             //Create
-            public static void CreateLogin(string email, string password) => SendToDatabase("INSERT INTO Logins VALUES (@password,@email)",
-                new ParamArgs("@email", email),
-                new ParamArgs("@password", password));
+            public static void CreateLogin(string email, string password) => Utils.Create(Table.Logins, new ParamArgs("@email", email), new ParamArgs("@pword", password));
             //Update
             public static void UpdateLogin(string email, string newPassword) => Utils.Update(Table.Logins,new ParamArgs("@email",email),new ParamArgs("@pword",newPassword));
             //Read
@@ -44,12 +57,9 @@ namespace HyggeFinal
             public static void DeleteLogin(string password) => Utils.Delete(Table.Logins,new ParamArgs("@pword",password));
         }
 
-
         public static class Industry {
             //Create
-            public static void CreateIndustry(string industryName, string field) => SendToDatabase("INSERT INTO Industry VALUES (@industryName,@field)",
-                new ParamArgs("@industryName", industryName),
-                new ParamArgs("@field", field));
+            public static void CreateIndustry(string industryName, string field) => Utils.Create(Table.Industry,new ParamArgs("@industryName",industryName),new ParamArgs("@field",field));
             //Update 
             public static void UpdateIndustry(string industryName, string newfield) => Utils.Update(Table.Industry,new ParamArgs("@industryName",industryName),new ParamArgs("@field",newfield));
             //Read
@@ -58,12 +68,9 @@ namespace HyggeFinal
             public static void DeleteIndustry(string industryName) => Utils.Delete(Table.Industry,new ParamArgs("@industryName",industryName));
         }
 
-
         public static class Education {
             //Create
-            public static void CreateEducation(string educationName, string locale) => SendToDatabase("INSERT INTO Education VALUES (@educationName,@locale)",
-                new ParamArgs("@educationName", educationName),
-                new ParamArgs("@locale", locale));
+            public static void CreateEducation(string educationName, string locale) => Utils.Create(Table.Education,new ParamArgs("@educationName",educationName),new ParamArgs("@locale",locale));
             //Update
             public static void UpdateEducation(string educationName, string newLocale) => Utils.Update(Table.Education,new ParamArgs("@educationName",educationName),new ParamArgs("@locale",newLocale));
             //Read
@@ -72,26 +79,22 @@ namespace HyggeFinal
             public static void DeleteEducation(string educationName) => Utils.Delete(Table.Education,new ParamArgs("@educationName",educationName));
         }
 
-
         public static class Person {
             //Create Person (should only create necessary data and the use update to add nonessentials)
             //public static void CreatePerson(
 
             //Update
             public static void UpdatePerson(string personID, ParamArgs changedValue) => Utils.Update(Table.Person,new ParamArgs("@personID",personID),changedValue);
-
             //Read
             public static DataSet ReadPerson(string personID) => Utils.Read(Table.Person,new ParamArgs("@personID",personID));
-
             //Delete
             public static void DeletePerson(string personID) => Utils.Delete(Table.Person, new ParamArgs("@personID", personID));
         }
 
-
         public static class Relationship {
             //Create
-            public static void CreateRelationship(string relationshipType, int lvlOfCommitment) => SendToDatabase("INSERT INTO Relationship VALUES (@relType,@lvlOfCommitment)",
-                new ParamArgs("@relType",relationshipType),
+            public static void CreateRelationship(string relationshipType, int lvlOfCommitment) => Utils.Create(Table.Relationship,
+                new ParamArgs("@relationshipType",relationshipType), 
                 new ParamArgs("@lvlOfCommitment",lvlOfCommitment));
             //Update
             public static void UpdateRelationship(string relationshipType, int newLvlOfCommitment) => Utils.Update(Table.Relationship,
@@ -100,35 +103,21 @@ namespace HyggeFinal
             //Read
             public static void ReadRelationship(string relationshipType) => Utils.Read(Table.Relationship,new ParamArgs("@relationshipType",relationshipType));
             //Delete
-            public static void DeleteRelationship(string relationshipType) => SendToDatabase("DELETE Relationship WHERE relationshipType = @relationshipType",
-                new ParamArgs("@relationshipType",relationshipType));
+            public static void DeleteRelationship(string relationshipType) => Utils.Delete(Table.Relationship,new ParamArgs("@relationshipType",relationshipType));
         }
-
 
         public static class Interest {
             //Create
-            public static void CreateInterest(string category, string group) => SendToDatabase("INSERT INTO Interest VALUES (@category,@group)",
-                new ParamArgs("@category",category),
-                new ParamArgs("@group",group));
+            public static void CreateInterest(string category, string group) => Utils.Create(Table.Interest,new ParamArgs("@interestCategory",category),new ParamArgs("@interestGroup",group));
             //Update
-            public static void UpdateInterest(string interestCategory, string newGroup) => SendToDatabase("UPDATE Interest SET interestGroup = @newGroup WHERE interestCategory = @category",
-                new ParamArgs("@category",interestCategory),
-                new ParamArgs("@newGroup",newGroup));
+            public static void UpdateInterest(string interestCategory, string newGroup) => Utils.Update(Table.Interest,
+                new ParamArgs("@interestCategory",interestCategory),
+                new ParamArgs("@interestGroup", newGroup));
             //Read
-            public static void ReadInterest(string interestCategory) => SendToDatabase("SELECT * FROM Interest WHERE interestCategory = @interestCategory", 
-                new ParamArgs("@interestCategory",interestCategory));
+            public static void ReadInterest(string interestCategory) => Utils.Read(Table.Interest,new ParamArgs("@interestCategory",interestCategory));
             //Delete
-            public static void DeleteInterest() => SendToDatabase("");
-        }
-        
-
-        public static string Test() { //this method should only be used to test out new features of the db. it does not test the functionality of the whole class.
-            try {
-                DataSet ds = Login.ReadLogin("anabanana@hotmail.com");
-                return ds.Tables[0].Rows[0][0].ToString(); // returns the value of the first column of the first row in string format
-            } catch (SqlException) { return ("failed to connect."); } // returns an error message if the client couldn't connect to the db server (check the data source!)
-        }
-
+            public static void DeleteInterest(string interestCategory) => Utils.Delete(Table.Interest, new ParamArgs("@interestCategory",interestCategory));
+        }        
 
         private static DataSet SendToDatabase(string sqlQuery, params ParamArgs[] args ) {
             try {
@@ -138,8 +127,9 @@ namespace HyggeFinal
                     using (SqlDataAdapter adapter = new SqlDataAdapter()) {
                         if (args != null) { foreach (ParamArgs param in args) command.Parameters.AddWithValue(param.ParamID, param.Value); } // fill each parameter using ParamArgs
                         string queryType = sqlQuery.Substring(0, 6);                                        //The initial command is separated into a substring
-                        if ( queryType == "UPDATE" || queryType == "DELETE" || queryType == "INSERT" ) {        // if the query writes to the database...
-                            switch (queryType) {// ... the adapter is associated with the proper command and executed.
+                        List<string> wca = new List<string>{ "UPDATE", "INSERT", "DELETE" };
+                        if (wca.Contains(queryType)) {        // if the query contains a command that writes to the database...
+                            switch (queryType) {// ... then the adapter will be associated with the proper command and executed.
                                 case "UPDATE":
                                     adapter.UpdateCommand = command; //association...
                                     adapter.UpdateCommand.ExecuteNonQuery(); //... and execution
