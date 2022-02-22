0a6812d65e82cc01e9e07dd7af63b40051eba6cf
da1552hu-s <75209747+da1552hu-s@users.noreply.github.com>
2022-02-22 16:58:42 +0100

    Update AdminAccess.Designer.cs

--- HyggeFinal/HyggeFinal/AdminAccess.Designer.cs
+++ HyggeFinal/HyggeFinal/AdminAccess.Designer.cs
@@ -28,39 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvTable = new System.Windows.Forms.DataGridView();
+            this.lblTable = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvTable
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 64);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(393, 374);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTable.Location = new System.Drawing.Point(12, 64);
+            this.dgvTable.Name = "dgvTable";
+            this.dgvTable.Size = new System.Drawing.Size(393, 374);
+            this.dgvTable.TabIndex = 0;
             // 
-            // label1
+            // lblTable
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lblTable.AutoSize = true;
+            this.lblTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTable.Location = new System.Drawing.Point(171, 37);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(58, 24);
+            this.lblTable.TabIndex = 1;
+            this.lblTable.Text = "Table";
+            this.lblTable.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // AdminAccess
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblTable);
+            this.Controls.Add(this.dgvTable);
             this.Name = "AdminAccess";
             this.Text = "AdminAccess";
             this.Load += new System.EventHandler(this.AdminAccess_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,7 +70,7 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvTable;
+        private System.Windows.Forms.Label lblTable;
     }
 }
